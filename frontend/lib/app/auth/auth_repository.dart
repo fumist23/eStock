7465fe729fd772bb835e0fe6abb396f9c2bbfe9b
Ryunosuke Aoki <ryunosuke1114@gmail.com>
2022-09-27 23:39:46 +0900

    fix : firebaseプロジェクトの変更

--- frontend/lib/app/auth/auth_repository.dart
+++ frontend/lib/app/auth/auth_repository.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_application_2/app/custom_exception.dart';
 import 'package:flutter_application_2/app/genaral_providers.dart';
-import 'package:flutter_application_2/app/repositories/custom_exception.dart';
+
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
