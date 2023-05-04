21875a30a570b73f07eaa9ab50d2a7d856b6421d
satofumi / Fumiya Sato <fumist1028@gmail.com>
2023-05-04 11:47:59 +0900

    Move db

--- backend/run.sh
+++ backend/run.sh
@@ -5,20 +5,20 @@ set -e
 echo "---- script started!! ----"
 
 # コンテナ起動時に持っているSQLiteのデータベースファイルは、後続処理でrestoreに成功したら削除したいのでrenameしておく
-if [ -f ./db/estock.db ]; then
-  mv ./db/estock.db ./db/estock.db.bk
+if [ -f ./estock.db ]; then
+  mv ./estock.db ./estock.db.bk
 fi
 
 # GCSからrestore
-litestream restore -if-replica-exists -config ./litestream.yaml ./db/estock.db
+litestream restore -if-replica-exists -config ./litestream.yaml ./estock.db
 
-if [ -f ./db/estock.db ]; then
+if [ -f ./estock.db ]; then
   # restoreに成功したら、renameしていたファイルを削除
   echo "---- Restored from Cloud Storage ----"
-  rm ./db/estock.db.bk
+  rm ./estock.db.bk
 else
   echo "---- Failed to restore from Cloud Storage ----"
-  mv ./db/estock.db.bk ./db/estock.db
+  mv ./estock.db.bk ./estock.db
 fi
 
 # メインプロセスでlitestreamの起動、サブプロセスにGoサーバーの起動を行う。
